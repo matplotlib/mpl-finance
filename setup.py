1afc37abab5ec694ad8b32beeb4eef142db61aac
Daniel Goldfarb <dgoldfarb.github@gmail.com>
2020-02-21 14:22:16 -0500

    update setup.py; begin deprecation process

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(name='mpl_finance',
-      version='0.10.0',
+      version='0.10.1',
       author='MPL Developers',
       author_email='matplotlib-users@python.org',
       py_modules=['mpl_finance'],
       description='Finance plots using matplotlib',
-      url='http://github.com/matplotlib/mpl_finance',
+      long_description=long_description,
+      long_description_content_type='text/markdown; charset=UTF-8',
+      url='http://github.com/matplotlib/mpl-finance',
       platforms='Cross platform (Linux, Mac OSX, Windows)',
       install_requires=['matplotlib'],
-      license="BSD",
-      classifiers=['Development Status :: 4 - Beta',
+      license="BSD-style",
+      classifiers=['Development Status :: 7 - Inactive',
                    'Programming Language :: Python :: 2',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
